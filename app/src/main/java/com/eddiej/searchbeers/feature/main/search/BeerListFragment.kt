967ec05d3a67a1e740f95c953a11acbaa789b8e9
Eddie <eddie@kornatus.com>
2021-11-30 17:50:53 +0900

    목록 갱신 시 스크롤 위치 초기화

--- app/src/main/java/com/eddiej/searchbeers/feature/main/search/BeerListFragment.kt
+++ app/src/main/java/com/eddiej/searchbeers/feature/main/search/BeerListFragment.kt
@@ -32,6 +32,7 @@ class BeerListFragment : BaseFragment<FragmentBeerListBinding>() {
         binding?.let {
             it.viewModel = viewModel
 
+            it.recyclerView.isVerticalScrollBarEnabled = true
             it.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
                 override fun onQueryTextSubmit(query: String?): Boolean {
                     if (!query.isNullOrEmpty()) {
@@ -53,10 +54,46 @@ class BeerListFragment : BaseFragment<FragmentBeerListBinding>() {
     private fun createAdapter(recyclerView: RecyclerView?, pagingData: PagingData<BeerItemEntity>) {
         recyclerView?.let { rv ->
             if (rv.adapter != null && rv.adapter is BeerListAdapter) {
-                rv.adapter as BeerListAdapter
+                val adapter = rv.adapter as BeerListAdapter
+                adapter.submitData(lifecycle, pagingData)
             } else {
                 // PagingAdapter
                 val adapter = BeerListAdapter(viewModel)
+
+                adapter.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
+                    override fun onChanged() {
+                        rv.scrollToPosition(0)
+                    }
+
+                    override fun onItemRangeChanged(positionStart: Int, itemCount: Int) {
+                        rv.scrollToPosition(0)
+                    }
+
+                    override fun onItemRangeChanged(
+                        positionStart: Int,
+                        itemCount: Int,
+                        payload: Any?
+                    ) {
+                        rv.scrollToPosition(0)
+                    }
+
+                    override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+                        rv.scrollToPosition(0)
+                    }
+
+                    override fun onItemRangeRemoved(positionStart: Int, itemCount: Int) {
+                        rv.scrollToPosition(0)
+                    }
+
+                    override fun onItemRangeMoved(
+                        fromPosition: Int,
+                        toPosition: Int,
+                        itemCount: Int
+                    ) {
+                        rv.scrollToPosition(0)
+                    }
+                })
+
                 // LoadingAdapter
                 val loadingAdapter = LoadingStateAdapter {
                     adapter.retry()
@@ -66,7 +103,8 @@ class BeerListFragment : BaseFragment<FragmentBeerListBinding>() {
                     loadingAdapter.loadState = loadStates.source.refresh
 
                     // 검색결과가 없을 때 간단한 텍스트 출력
-                    val isEmpty =  (loadStates.source.refresh is LoadState.NotLoading && adapter.itemCount == 0)
+                    val isEmpty =
+                        (loadStates.source.refresh is LoadState.NotLoading && adapter.itemCount == 0)
                     binding?.textEmpty?.isVisible = isEmpty
                 }
 
